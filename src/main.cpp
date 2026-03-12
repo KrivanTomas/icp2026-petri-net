5dd12d19708249b5b7c3855bee64784dab0521dd
Tomáš Křivan <cyan.roke@gmail.com>
2026-03-12 13:45:10 +0100

    Qt5 Widgets test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
+// src/main.cpp
+// Authors:
+//  Tomáš Křivan
+//
 #include <iostream>
-#include "module_test.hpp"
+#include <QApplication>
+#include "module_test.h"
+#include "main_window.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout << "Hello, World!\n";
     module_test();
-    return 0;
+
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
+    return app.exec();
 }
