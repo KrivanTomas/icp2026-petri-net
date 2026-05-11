90a2ce1169540bdc50a80c698b199597e98e64a4
Tomáš Kudera <xkudert00@stud.fit.vut.cz>
2026-05-11 21:20:00 +0200

    Update src/model/place.cpp
    repaired camelcase for setLastTimeChange
    
    Co-authored-by: Tomáš Křivan <114081566+KrivanTomas@users.noreply.github.com>

--- src/model/place.cpp
+++ src/model/place.cpp
@@ -42,6 +42,6 @@ int Place::getLastTimeChange() const {
     return m_last_change_time_ms;
 }
 
-void Place::setLasttimeChange(int time_ms) {
+void Place::setLastTimeChange(int time_ms) {
     m_last_change_time_ms = time_ms;
 }
