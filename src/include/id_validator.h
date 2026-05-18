4e0bf5fbfc80ecaf93ae7ba4a0fea47cd18c99d2
Tomáš Křivan <cyan.roke@gmail.com>
2026-05-18 20:56:59 +0200

    docs(doxygen): add doxygen comments

--- src/include/id_validator.h
+++ src/include/id_validator.h
@@ -1,7 +1,7 @@
 /**
  * @file id_validator.h
  *
- * @brief Header for the editor id input validation logic
+ * @brief ModelIdValidator definition 
  *
  * @author
  *     Tomáš Křivan
@@ -11,14 +11,30 @@
 
 #include <QValidator>
 
-
+/**
+ * @brief Helps validate input field for ids
+ */
 class ModelIdValidator : public QValidator {
     Q_OBJECT
 public:
+    /**
+     * @brief Validator specialization
+     */
     enum class IdType { Place, Transition, Arc };
 
+    /**
+     * @brief Create a specialized validator for the required type of id
+     */
     ModelIdValidator(IdType type, QObject *parent = nullptr);
-    
+
+    /**
+     * @brief Check if the new id does not colide with any existing id
+     *
+     * @param input
+     * @param pos
+     *
+     * @return validation state
+     */
     State validate(QString &input, int &pos) const override;
 private:
     IdType id_type;
--- src/include/main_window.h
+++ src/include/main_window.h
@@ -1,7 +1,7 @@
 /**
  * @file main_window.h
  *
- * @brief Header file for the main gui window
+ * @brief MainWindow definition 
  *
   * @author
  *     Tomáš Křivan
@@ -24,33 +24,94 @@ namespace Ui {
 };
 QT_END_NAMESPACE
 
-
+/**
+ * @brief The main window of the GUI editor
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
 public:
+    /**
+     * @brief Create ui and other objects needed for the editor
+     *
+     * @param parent
+     */
     MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Free ui and all objects used in the editor
+     *
+     * @param parent
+     */
     ~MainWindow();
 
 public slots:
+    /**
+     * @brief Set editor mode
+     *
+     * @param mode
+     */
     void onEditorModeChanged(EditorGraphicsScene::Mode mode);
+
+    /**
+     * @brief Enables property edit and delete actions if applicable
+     */
     void onEditorSelectionChanged();
+
+    /**
+     * @brief Delete selected items
+     */
     void onEditorDeleteSelection();
+
+    /**
+     * @brief Reset any current edited net
+     */
     void newFile();
+
+    /**
+     * @brief Open and load a net by replacing current edited net
+     */
     void openFile();
+
+    /**
+     * @brief Save the current edited net
+     */
     void saveFile();
 
 protected:
+    /**
+     * @brief Selection type
+     */
     enum class SelectedItemType { None, Place, Transition, Arc};
+
+    /**
+     * @brief Currently selected type
+     */
     SelectedItemType selected_item_type = SelectedItemType::None;
+
+    /**
+     * @brief Pointers to the currently selected item
+     */
     union {
         QGraphicsItem *item;  
         EditorPlaceItem *place_item;  
         EditorTransitionItem *transition_item;  
         EditorArcItem *arc_item;  
     } selected_item;
+
+    /**
+     * @brief React to a user closing the app
+     *
+     * @param event
+     */
     void closeEvent(QCloseEvent *event) override;
+
+    /**
+     * @brief Enables the property editor widget, used when exactly one item is selected
+     *
+     * @param item
+     */
     void setSelectedItem(QGraphicsItem *item = nullptr);
 
 private:
